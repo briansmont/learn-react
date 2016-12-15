5f7b465b655e68550c5b884ffde19020a0a1a972
Brian Mont <briansmont830@gmail.com>
2016-12-15 19:15:47 +0000

    added onSubmit event handler with the form, used refs object to capture user name input as onButtonClick function var name

--- helloreact/public/app.jsx
+++ helloreact/public/app.jsx
@@ -5,15 +5,26 @@ var Greeter = React.createClass({
       message: 'Default message!'
     };
   },
+  onButtonClick: function(e) {
+    e.preventDefault();
+    var name = this.refs.name.value;
+    alert(name);
+  },
   render: function() {
     var name = this.props.name;
     var message = this.props.message;
     return (
       <div>
         <h1>Hello {name}!</h1>
-        <p>{message + '!'}</p>
+        <p>{message}</p>
+        
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
+        
       </div>
-      );
+    );
   }
 });
 
