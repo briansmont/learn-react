34a6f555235b6b800832a9d392647a3fb84a180d
Brian Mont <briansmont830@gmail.com>
2016-12-15 18:58:27 +0000

    basic prop usage

--- helloreact/public/app.jsx
+++ helloreact/public/app.jsx
@@ -1,16 +1,27 @@
 var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'Default message!'
+    };
+  },
   render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
     return (
       <div>
-        <h1>Hello React!</h1>
-        <p>This is from the Greeter component!</p>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!'}</p>
       </div>
       );
   }
 });
 
+var firstName = 'Brian';
+var newMessage = 'Today is a day!';
+
 
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message={newMessage}/>,
   document.getElementById('app')
 );
