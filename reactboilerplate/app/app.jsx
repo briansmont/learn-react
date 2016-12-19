b2e25aac648fdff4ec34301c681e38e7c94716c0
Brian Mont <briansmont830@gmail.com>
2016-12-19 15:21:19 +0000

    install stage-0

--- reactboilerplate/app/app.jsx
+++ reactboilerplate/app/app.jsx
@@ -1,8 +1,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {
+  name: 'Brian',
+  location: 'Philadelphia',
+};
 
+var objTwo = {
+  age: '28',
+  ...objOne
+};
 
+alert(objTwo);
 
 ReactDOM.render(
   <h1>Boilerplate app!</h1>,
